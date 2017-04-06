921f96a3e7fa49cfd04fbfe0c8f3d603d0cdf05c
Fırat Yılmaz <firatyilmaz@live.com>
2017-04-06 17:34:33 +0300

    Add project files.

---
+++ signalR/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace signalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message) {
+            //Clients.All();//bütün istyemciler de tetikle
+            //Clients.Others(); //Diğer clientlerde
+            //Clients.Caller();//bende
+            //Clients.Users("");//bewlirtilen kullanıcıya
+
+
+
+            //Burda viewdan gönderilen sunucudaki methotları tetikliyoruz
+            Clients.Others.GetMessageOther(name,message);//karşı taraflara name ve message
+            Clients.Caller.GetMessageCaller(message);//Mesajı yazan kişiye kendi adını göstermiyoruz
+
+            //Datamız sunucuya çıktı sıra clientlere iletmek >>>Index
+        }
+    }
+}
